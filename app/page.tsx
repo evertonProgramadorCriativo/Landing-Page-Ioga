82792343d845ef9af1d39575e421195d43019ee7
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2026-04-30 22:37:30 -0300

    feat : Criando Seção Certificado

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,14 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
+import HeroSection from "@/components/HeroSection";
+import CertificadosSection from "@/components/CertificadosSection";
 export default function Home() {
   return (
     <main>
       <Header />
 
-      <div className="text-center bg-[url(/image.png)] bg-cover bg-top h-238.25 flex items-center justify-center">
-        <div className="flex items-center justify-center flex-col bg-opacity-50 p-6 rounded-lg">
-          <h1 className="text-6xl leading-15  font-semibold text-white  font-syncopate">
-            Yoga for Body & Mind
-          </h1>
-          <span className="text-white text-2xl mt-12 mb-4 text-white-600 line-height-6">
-            Find balance in the heart of the city
-          </span>
-          <button
-            className="mt-12 bg-transparent text-white"
-            style={{
-              border: "2px solid white",
-              padding: "20px 92px",
-              borderRadius: "88px",
-              fontSize: "20px",
-            }}
-          >
-            Book class
-          </button>
-        </div>
-      </div>
-
+      <HeroSection />
+      <CertificadosSection />
       <Footer />
     </main>
   );
---
+++ components/CertificadosSection.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+const cards = [
+  {
+    id: 1,
+    image: "/uil-flower.png",
+    title: "Certified Instructors",
+    description: "Expert guidance for your practice",
+  },
+  {
+    id: 2,
+    image: "/uil-award.png",
+    title: "Eco-friendly Materials",
+    description: "Sustainable and natural",
+  },
+  {
+    id: 3,
+    image: "/Frame-33.png",
+    title: "Multiple Yoga Styles",
+    description: "Find your perfect practice",
+  },
+  {
+    id: 4,
+    image: "/chicara-cafe.png",
+    title: "Relaxation Zone",
+    description: "Tea and community space",
+  },
+];
+
+export default function CertificadosSection() {
+  return (
+    <section className="h-238.25 flex items-center justify-center">
+      <div className="flex items-center justify-center flex-col bg-opacity-50 p-6 rounded-lg">
+        <h2 className="text-6xl flex text-center leading-18  font-semibold text-black  font-syncopate">
+          The Santosha <br /> Yoga Space{" "}
+        </h2>
+        <p
+          className="text-black text-3xl mt-12 mb-4 text-black-600 line-height-6"
+          style={{ padding: "0 297px", textAlign: "center" }}
+        >
+          santosha is a modern yoga studio where everyone can find their perfect
+          practice. We&apos;ve created a welcoming space with professional
+          instructors to help you achieve harmony, strength, and flexibility.
+          Our classes suit all levels ,from beginners to advanced practitioners.
+        </p>
+
+        <div
+          className="flex  items-center justify-around "
+          style={{ width: "80%", marginTop: "5%" }}
+        >
+          {cards.map((card) => (
+            <div
+              key={card.id}
+              className="flex flex-col items-center justify-center"
+            >
+              <Image
+                src={card.image}
+                alt={card.title}
+                width={80}
+                height={80}
+                className="rounded-lg"
+              />
+
+              <h3 className="text-xl font-semibold mt-4 text-center">
+                {card.title}
+              </h3>
+
+              <span className="text-gray-400 text-center">
+                {card.description}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
